b048b4e423bfba4d134ff8f72a3945b49889984c
B. A. Bhathiya N. H. Perera <jadogg.coder user name to domain gmail.com>
2013-08-14 17:21:07 +0530

    FIRST COMMIT

---
+++ CmdWrapper/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CmdWrapper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.Filter = "Icon File (*.ico)|*.ico";
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK) 
+            {
+                pictureBox1.ImageLocation = openFileDialog1.FileName;
+            }
+            
+        }
+    }
+}
